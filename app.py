38451dda3fd96c4a20978962cadc727ff7b1d73b
Vatsal-1501 <vatsal@on2cook.com>
2025-04-25 17:33:08 +0530

    SVM model added

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+# Load models
+status_model = joblib.load("models/pan_classifier.pkl")
+scaler = joblib.load("models/scaler.pkl")
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "On2Cook AI Server is Running!"})
+
+@app.route("/detect", methods=["POST"])
+def detect_pan_status():
+    try:
+        data = request.json
+        features = np.array(data["features"]).reshape(1, -1)
+        
+        # Convert to DataFrame with feature names
+        feature_names = ['PAN_Inside:', 'PAN_Outside:', 'Glass_Temp:']
+        features_df = pd.DataFrame(features, columns=feature_names)
+        
+        # Scale input features
+        features_scaled = scaler.transform(features_df)
+        
+        # Predict pan status
+        status = status_model.predict(features_scaled)[0]
+        
+        return jsonify({"pan_status": "Not Empty" if status == 0 else "Empty"})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
